64afdc9d10dc92c1b2685b434bed9b9283e8d3c3
Pranshu <pranshgupta54@gmail.com>
2023-07-09 02:16:30 +0530

    Fetch on start only

--- routes/allContests.js
+++ routes/allContests.js
@@ -6,26 +6,35 @@ const geeksforgeeksService = require('../services/geeksforgeeks');
 const codechefService = require('../services/codechef');
 const date = require("../date.js");
 
-router.get("/", async function (req, res) {
-    try {
-      const day = date.getDate()
-      const cflist = await codeforcesService.codeforces_c();
-      const llist = await leetCodeService.leetcode_c();
-      const gfglist = await geeksforgeeksService.geeksforgeeks_c();
-      const cclist = await codechefService.codechef_c();
-      console.log("FILTERED:");
-    //   console.log(cflist);
-    //   console.log(llist);
-    //   console.log(llist.length);
-      const finalList = [...cflist, ...llist, ...gfglist, ...cclist];
-      finalList.sort((a, b) => a.startTimeSeconds - b.startTimeSeconds);
-      console.log(finalList);
-      res.render("codeforces.ejs", {date: day, list:finalList});
-      // res.send(list);
-    } catch (error) {
-      console.log("Error:", error);
-      res.status(500).send("Internal Server Error");
-    }
+let contestList = [];
+
+async function fetchContests() {
+  try {
+    const cflist = await codeforcesService.codeforces_c();
+    const llist = await leetCodeService.leetcode_c();
+    const gfglist = await geeksforgeeksService.geeksforgeeks_c();
+    const cclist = await codechefService.codechef_c();
+
+    contestList = [...cflist, ...llist, ...gfglist, ...cclist];
+    contestList.sort((a, b) => a.startTimeSeconds - b.startTimeSeconds);
+
+    console.log("Contests fetched successfully.");
+  } catch (error) {
+    console.log("Error fetching contests:", error);
+  }
+}
+
+// Call the function to fetch contests when the server starts
+fetchContests();
+
+router.get("/", function (req, res) {
+  try {
+    const day = date.getDate();
+    res.render("codeforces.ejs", { date: day, list: contestList });
+  } catch (error) {
+    console.log("Error:", error);
+    res.status(500).send("Internal Server Error");
+  }
 });
 
 module.exports = router;
