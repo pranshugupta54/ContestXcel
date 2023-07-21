d1dbc5c0d306ffbe3b0f6fa83c4cc4443c1fd8c8
Pranshu <pranshgupta54@gmail.com>
2023-07-21 23:44:31 +0530

    user-icon

--- routes/contest/allContests.js
+++ routes/contest/allContests.js
@@ -11,14 +11,7 @@ let timing = {};
 
 async function fetchContests() {
   try {
-    const cflist = await codeforcesService.codeforces_c();
-    const llist = await leetCodeService.leetcode_c();
-    const gfglist = await geeksforgeeksService.geeksforgeeks_c();
-    const cclist = await codechefService.codechef_c();
-
-    contestList = [...cflist, ...llist, ...gfglist, ...cclist];
-    contestList.sort((a, b) => a.startTimeSeconds - b.startTimeSeconds);
-    
+    contestList = [];
     const now = new Date();
     const fetchTime = now.toLocaleString("en-US", {
       day: "numeric",
@@ -32,11 +25,50 @@ async function fetchContests() {
 
     timing = { fetchTime };
 
-    console.log("Contests fetched successfully.");
+    // Fetch contests from codeforcesService
+    try {
+      const cflist = await codeforcesService.codeforces_c();
+      contestList.push(...cflist);
+      console.log("Codeforces contests fetched successfully.");
+    } catch (error) {
+      console.log("Error fetching Codeforces contests:", error);
+    }
+
+    // Fetch contests from leetCodeService
+    try {
+      const llist = await leetCodeService.leetcode_c();
+      contestList.push(...llist);
+      console.log("LeetCode contests fetched successfully.");
+    } catch (error) {
+      console.log("Error fetching LeetCode contests:", error);
+    }
+
+    // Fetch contests from geeksforgeeksService
+    try {
+      const gfglist = await geeksforgeeksService.geeksforgeeks_c();
+      contestList.push(...gfglist);
+      console.log("GeeksforGeeks contests fetched successfully.");
+    } catch (error) {
+      console.log("Error fetching GeeksforGeeks contests:", error);
+    }
+
+    // Fetch contests from codechefService
+    try {
+      const cclist = await codechefService.codechef_c();
+      contestList.push(...cclist);
+      console.log("CodeChef contests fetched successfully.");
+    } catch (error) {
+      console.log("Error fetching CodeChef contests:", error);
+    }
+
+    contestList.sort((a, b) => a.startTimeSeconds - b.startTimeSeconds);
+
+    console.log("All contests fetched successfully.");
   } catch (error) {
     console.log("Error fetching contests:", error);
   }
 }
+
 
 // Call the function to fetch contests when the server starts
 fetchContests();
@@ -46,6 +78,7 @@ router.get("/", function (req, res) {
   try {
     const day = date.getDate();
     res.render("contests.ejs", { date: day, list: contestList, fetchTime: timing.fetchTime });
+    console.log(contestList);
   } catch (error) {
     console.log("Error:", error);
     res.status(500).send("Internal Server Error");
